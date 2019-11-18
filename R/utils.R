f30ef5261941042070d518ad078632f3efe5eda8
Claudio Reggiani <nophiq@gmail.com>
2019-11-18 15:23:46 +0100

    Added scale_this function

--- R/utils.R
+++ R/utils.R
@@ -12,3 +12,14 @@ get_formula <- function(col.names, class.idx=1) {
 as_charnum <- function(x) {
   as.numeric(as.character(x))
 }
+
+#' Scaling function
+#'
+#' @export
+scale_this <- function(x, method="scale") {
+  ret <- NULL
+  if (method == "std") ret <- (x - mean(x, na.rm=TRUE)) / sd(x, na.rm=TRUE)
+  if (method == "norm") ret <- (x - mean(x, na.rm=TRUE)) / (max(x, na.rm=TRUE) - min(x, na.rm=TRUE))
+  if (method == "scale") ret <- (x - min(x, na.rm=TRUE)) / (max(x, na.rm=TRUE) - min(x, na.rm=TRUE))
+  ret
+}
