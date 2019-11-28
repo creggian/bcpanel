759c441e2109362272254cf4a598bfb0776b03f1
Claudio Reggiani <nophiq@gmail.com>
2019-11-28 23:48:10 +0100

    Added majority_undersampling_bool function

--- R/unbalanced.R
+++ R/unbalanced.R
@@ -2,6 +2,14 @@
 #'
 #' @export
 majority_undersampling <- function(x, target) {
+  bool <- majority_undersampling_bool(x = x, target = target)
+  x[bool,]
+}
+
+#' Majority undersampling booleans
+#' 
+#' @export
+majority_undersampling_bool <- function(x, target) {
   y <- x[[target]]
   t <- table(y)
   minority_class_idx <- which.min(t)
@@ -9,11 +17,12 @@ majority_undersampling <- function(x, target) {
   minority_class <- names(t)[minority_class_idx]
   majority_class <- names(t)[majority_class_idx]
   
-  slice_minority <- x[y == minority_class,]
+  minority_bool <- y == minority_class
   
-  slice_majority <- x[y == majority_class,]
-  s <- sample(1:nrow(slice_majority), nrow(slice_minority), replace = FALSE)
-  undersampling_majority <- slice_majority[s,]
+  majority_bool <- y == majority_class
+  majority_idx <- which(majority_bool)
+  slice_majority_idx <- sample(majority_idx, min(t), replace = FALSE)
+  slice_majority_bool <- (1:nrow(x)) %in% slice_majority_idx
   
-  rbind(slice_minority, undersampling_majority)
+  minority_bool | slice_majority_bool
 }
--- tests/testthat/test-unbalanced.R
+++ tests/testthat/test-unbalanced.R
@@ -9,6 +9,7 @@ test_that("majority_undersampling", {
   )
   
   object <- majority_undersampling(x = df, target = "y")
+  expect_equal(class(object), "data.frame")
   expect_equal(nrow(object), 4)
   expect_equal(ncol(object), 3)
   expect_equal(length(which(object$y == 1)), 2)
@@ -22,8 +23,37 @@ test_that("majority_undersampling", {
   )
   
   object <- majority_undersampling(x = df, target = "y")
+  expect_equal(class(object), "data.frame")
   expect_equal(nrow(object), 2)
   expect_equal(ncol(object), 3)
   expect_equal(length(which(object$y == 1)), 1)
   expect_equal(length(which(object$y == 0)), 1)
+})
+
+test_that("majority_undersampling_bool", {
+  df <- data.frame(
+    c1 = c(1, 2, 3, 4, 5.8),
+    c2 = c("a", "b", "c", "d", "e"),
+    y = factor(c(1, 1, 0, 0, 0)),
+    stringsAsFactors = FALSE
+  )
+  
+  object <- majority_undersampling_bool(x = df, target = "y")
+  expect_equal(class(object), "logical")
+  expect_equal(length(object), 5)
+  expect_equal(length(which(object == TRUE)), 4)
+  expect_equal(length(which(object == FALSE)), 1)
+  
+  df <- data.frame(
+    c1 = c(1, 2, 3, 4, 5.8),
+    c2 = c("a", "b", "c", "d", "e"),
+    y = factor(c(1, 1, 1, 1, 0)),
+    stringsAsFactors = FALSE
+  )
+  
+  object <- majority_undersampling_bool(x = df, target = "y")
+  expect_equal(class(object), "logical")
+  expect_equal(length(object), 5)
+  expect_equal(length(which(object == TRUE)), 2)
+  expect_equal(length(which(object == FALSE)), 3)
 })
